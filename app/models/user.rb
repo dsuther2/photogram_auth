c76c70b6dcded858f932a010fb02a294cba17589
dsutherl <dsutherl>
2016-11-22 09:57:46 -0600

    Validation Rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, :presense => true, :uniqueness => true
+  #validate username is present and unique
+  validates :username, :presence => true, :uniqueness => true
 end
